331c4ab07efdee256857c54d6a50b912953c89e1
Keila2Vacca <knvaccab@ufpso.edu.co>
2024-05-15 17:17:12 -0500

    terminado

---
+++ taller2/src/components/vue/Calculator.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex-col bg-orange-100 rounded-lg py-8 px-6 w-96 shadow-md flex">
+        <div class="relative">
+            <div class="absolute top-0 left-0 flex justify-center items-center">
+                <div class=" h-5 w-12 rounded-lg bg-white">
+                    <img class="size-5 rounded-full" src="https://e7.pngegg.com/pngimages/181/188/png-clipart-logo-circle-area-brand-font-cartoon-sun-cartoon-character-text.png" alt="sol logo">
+                </div>
+                <span class="mx-2 text-gray-500 text-sm font-semibold">Switch to Dark</span>
+            </div>
+            <p class="mt-6 text-right text-2xl text-gray-500 font-semibold">45 x 24</p>
+            <p class="text-right text-5xl font-bold">1080</p>
+        </div>
+        <div class="mt-8 grid grid-cols-4 gap-50px">
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">7</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">8</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">9</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">/</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">4</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">5</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">6</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">*</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">1</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">2</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">3</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">-</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">0</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">.</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">+</button>
+            <button class="h-50px bg-orange-300 text-3xl text-center font-bold border-black border-2 py-5">=</button>
+        </div>
+    </div>    
+</template>
